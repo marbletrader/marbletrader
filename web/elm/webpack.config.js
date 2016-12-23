7767f821e74984d82c0016e5555d0457195bbd24
Lachlan Kermode <lachlankermode@live.com>
2016-12-23 18:13:33 +1300

    change frontend output to web/public

--- web/elm/webpack.config.js
+++ web/elm/webpack.config.js
@@ -6,7 +6,7 @@ var autoprefixer      = require( 'autoprefixer' );
 var ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 var CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 var entryPath         = path.join( __dirname, 'src/static/index.js' );
-var outputPath        = path.join( __dirname, 'dist' );
+var outputPath        = path.join( __dirname, '../public' );
 
 console.log( 'WEBPACK GO!');
 
