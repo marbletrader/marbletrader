ae6cdc30c323295f7df0a7012ea6eeef890ebc18
Lachlan Kermode <lachlankermode@live.com>
2016-12-30 19:06:15 -0500

    Add redux architecture, folder structure

--- web/client/src/index.js
+++ web/client/src/index.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import allReducers from './reducers'
+import App from './containers/App'
+import './index.css'
+
+const store = createStore(allReducers)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
-);
+)
